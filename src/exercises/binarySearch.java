8e6116b2c664ce9ca57fac6e2f9cb5cdf606905d
Dana <Dana.ackerman0914@gmail.com>
2024-06-01 11:37:49 +0500

    Iterative binary search implementation

---
+++ src/exercises/binarySearch.java
@@ -0,0 +1,26 @@
+package exercises;
+
+public class binarySearch {
+    public static void main(String[] args) {
+        int[] arr = {0, 4, 5, 6, 7, 9, 10, 15, 21, 47, 55, 67, 78, 79, 85, 91, 96, 100};
+        int elem = 85;
+        System.out.println(getBinarySearch(arr, elem, 0, arr.length));
+
+    }
+    public static int getBinarySearch(int[] arr, int elem, int l, int r) {
+        int index = -1;
+        while (l <= r) {
+            int mid = l + (r-l) / 2;
+            if (elem == arr[mid]) {
+                index = mid;
+                break;
+            }
+            if (arr[mid] < elem) {
+                l= mid+1;
+            } else {
+                r=mid-1;
+            }
+        }
+        return index;
+    }
+}
