ef665647cf91e8485d94427d20a1dbb8629cfdbf
Dana <Dana.ackerman0914@gmail.com>
2024-06-01 11:37:13 +0500

    Binary search with recursion

---
+++ src/exercises/RecursiveBinarySearch.java
@@ -0,0 +1,25 @@
+package exercises;
+
+public class RecursiveBinarySearch {
+    public static void main(String[] args) {
+        int[] arr = {0, 4, 5, 6, 7, 9, 10, 15, 21, 47, 55, 67, 78, 79, 85, 91, 96, 100};
+        int elem = 78;
+        System.out.println(getBinarySearch(arr, elem, 0, arr.length));
+    }
+
+    public static int getBinarySearch(int[] arr, int elem, int l, int r) {
+        if (l <= r) {
+            int m = l + (r - l) / 2;
+            if (elem == arr[m]) {
+                return m;
+            }
+            if (arr[m] < elem) {
+                return getBinarySearch(arr, elem, m + 1, r);
+            } else {
+                return getBinarySearch(arr, elem, l, m - 1);
+            }
+        }
+        return -1;
+
+    }
+}
